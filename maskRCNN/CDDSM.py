b88777025bdc8ff953e2c1513a61576ad3eda18c
himanshuupreti <himanshu@github.com>
2018-11-19 08:01:40 +0530

    maskRCNN next step

---
+++ maskRCNN/CDDSM.py
@@ -0,0 +1,113 @@
+from __future__ import print_function, division
+import pydicom
+import torch
+import os
+import sys
+import pandas as pd
+import numpy as np
+import pydicom as DCM
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from PIL import Image, ImageOps, ImageEnhance
+from sklearn.preprocessing import LabelEncoder
+from skimage import img_as_float
+import torchvision
+import torch
+from sklearn.feature_extraction.image import extract_patches_2d
+import PIL
+from skimage.util import img_as_bool
+
+
+
+class MammographyDataset(Dataset):
+    """Creating CBIS-DDSM pytorch dataset."""
+
+    def __init__(self, csv_file, root_dir,img_size):
+        """
+        Args:
+            csv_file (string): Path to the csv file containing labels.
+            root_dir (string): path to CuratedDDSM directory.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        #Image size
+        self.img_size = img_size
+        # Transforms
+        self.to_tensor = transforms.ToTensor()
+        # Read the csv file
+        self.frame = pd.read_csv(csv_file)
+        # Image and mask Columns
+        self.image_arr = np.asarray(self.frame['image file path'])
+        self.mask_arr = np.asarray(self.frame['ROI mask file path'])
+        # Labels
+        self.label_arr = np.asarray(self.frame['pathology_class'])
+        # Calculate Len
+        self.data_len = len(self.frame.index)
+        # Location of Curated DDSM
+        self.root_dir = root_dir
+        # Transformations to convert image into 512*512 / chabge it to random rotate
+        # self.transformations =             transforms.Compose([transforms.Grayscale(1),
+        #                        transforms.Resize(img_size,interpolation=Image.LANCZOS),
+        #                        transforms.CenterCrop(img_size)])
+
+    def __len__(self):
+        return self.data_len
+
+    def __getitem__(self, index):
+        # Open image
+        #img_as_img = Image.open(single_image_name)
+        image_path = os.path.join(self.root_dir,self.image_arr[index])
+        mask_path =  os.path.join(self.root_dir,self.mask_arr[index])
+
+        img= DCM.read_file(image_path).pixel_array.astype(float)
+        mask = DCM.read_file(mask_path).pixel_array.astype(float)
+        img = (np.maximum(img,0)/ img.max()) * 255.0
+        img = np.uint8(img)
+        mask = (np.maximum(mask,0)/mask.max())
+        
+        #patches formation
+        
+        patches = extract_patches_2d(img, (self.img_size,self.img_size),max_patches=21, random_state=5)
+        maskpatches = extract_patches_2d(mask,(self.img_size,self.img_size),max_patches=21,random_state=5)
+        labels=[]
+        for eachmask in maskpatches:
+            labels.append(img_as_bool(eachmask).any())
+        labels = [int(elem) for elem in labels]
+        labels = np.asarray(labels)
+
+        patches_tensor =  torch.from_numpy(patches)
+        patches_tensor = patches_tensor.unsqueeze(1)
+        labels_tensor = torch.from_numpy(labels)
+
+        img_tensor = torch.from_numpy(img) 
+       
+        image_label = self.label_arr[index]
+
+        return (patches_tensor, labels_tensor,image_label,img)
+    
+    
+
+
+
+class FullTrainingDataset(Dataset):
+    def __init__(self, full_ds, offset, length):
+        self.full_ds = full_ds
+        self.offset = offset
+        self.length = length
+        # super(FullTrainingDataset, self).init()
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, i):
+        return self.full_ds[i+self.offset]
+
+
+def trainValSplit(dataset, val_share):
+    val_offset = int(len(dataset)*(1-val_share))
+    
+    return FullTrainingDataset(dataset, 0, val_offset), FullTrainingDataset(dataset, val_offset, len(dataset)-val_offset)
+
+
+
---
+++ maskRCNN/main.py
@@ -0,0 +1,418 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[64]:
+
+
+import os
+import sys
+
+import argparse
+import shutil
+import time
+
+import math
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+import torchvision.models as models
+
+from torchviz import make_dot
+import matplotlib.pyplot as plt
+import graphviz
+
+from pathlib import Path
+from userFunctions import *
+from userClass import *
+
+# Model Specifics
+# import pytorch_resnet as R
+import BaseModel as B
+import CDDSM
+from tqdm import tqdm,tqdm_notebook
+
+import logging
+import time
+# # TensorBoard Logger
+
+# In[76]:
+
+
+# from tensorboardX import SummaryWriter
+# writer = SummaryWriter('runs',comment="pretrained")
+
+
+suffix = time.strftime("%d%b%Y%H%M",time.localtime())
+logtime = time.strftime("%d%b%Y %H:%M:%S",time.localtime())
+
+level = logging.INFO
+format = '%(message)s'
+handlers = [logging.FileHandler('./logs/Run{}.log'.format(suffix)),logging.StreamHandler()]
+logging.basicConfig(level=level,format=format,handlers=handlers)
+
+
+logging.info("*****************PrerTrained Network Testing******************\n")
+# x = torch.randn(batch_size, channels_mammo,heights_mammo , width_mammo)
+
+
+# # Reading Standard CSV files by TCIA for test/train
+
+
+#Device Selection
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# device ='cpu'
+# Hyper parameters
+num_epochs = 100
+num_classes = 3
+batch_size = 5
+learning_rate = 0.001
+
+total_iteration = 10000
+img_resize =H=W=512
+
+
+homedir = str(Path.home())
+homedir
+
+train_df = CDDSM.createTrainFrame(homedir)
+test_df = CDDSM.createTestFrame(homedir)
+mammogram_dir = '/home/himanshu/CuratedDDSM/'
+train_file = mammogram_dir+'train.csv'
+test_file = mammogram_dir+'test.csv'
+train_df.to_csv(train_file)
+test_df.to_csv(test_file)
+
+classes = ('BENIGN', 'BENIGN_WITHOUT_CALLBACK', 'MALIGNANT')
+                
+
+
+# # Making of CBIS-DDSM Dataset (train,val,test)
+
+# In[66]:
+
+
+dataset =  CDDSM.MammographyDataset(train_file,homedir,img_resize)
+test_dataset = CDDSM.MammographyDataset(test_file,homedir,img_resize)
+
+train_dataset , val_dataset = CDDSM.trainValSplit(dataset,val_share=0.1)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
+
+# # Length of each Dataset
+
+# In[67]:
+
+
+numberOfTrainData = train_dataset.__len__()
+numberOfValData = val_dataset.__len__()
+numberOfTestData =  test_dataset.__len__()
+
+total_step=len(train_loader)
+
+logging.info('Size of training dataset {}'.format(numberOfTrainData))
+logging.info('Size of Validation dataset {}'.format(numberOfValData))
+logging.info('Size of testing dataset {}'.format(numberOfTestData))
+logging.info('No. of Epochs: {}\n Batch size: {}\n Learning_rate : {}\n Image size {}*{}\n Step {}'
+        .format(num_epochs,batch_size,learning_rate,H,W,total_step))
+
+
+# # Checking images in each dataset by making grid
+
+# # Get Model
+
+# In[71]:
+
+
+
+resnet = models.resnet152(pretrained=True)
+
+
+# removing last layer of resnet and grad false
+features_len = resnet.fc.in_features
+# print(features_len)
+# pause('Features')
+resnet = nn.Sequential(*list(resnet.children())[:-1])
+
+
+# for param in resnet.parameters():
+#     param.requires_grad = False
+    
+    
+class myCustomModel(torch.nn.Module):
+    def __init__(self,pretrainedModel,features_len):
+        super(myCustomModel,self).__init__()
+        
+        self.layer0 = nn.Sequential()
+        self.layer0.add_module('conv0',nn.Conv2d(1,3,kernel_size=9,stride=1,padding=0,dilation=8))
+        self.layer0.add_module('relu0',nn.ReLU())
+        self.layer0.add_module('maxpool',nn.MaxPool2d(kernel_size=2))
+        self.layer1 = nn.Sequential()
+        self.layer1.add_module('pretrained',pretrainedModel)
+        self.fc = nn.Linear(in_features=features_len,out_features=3)
+    def forward(self,x):
+        x = self.layer0(x)
+        features = self.layer1(x)
+        features = features.view(features.size(0), -1)
+        x =  self.fc(features)
+        return features , x
+
+def getCustomPretrained(model,features_len):
+    return myCustomModel(model,features_len)
+    
+    
+# parameters with parameters requires grad is True
+# for p in resnet18.parameters():
+#     print(p.requires_grad)
+
+# model = B.getModel(3).to(device)
+
+
+# In[72]:
+
+
+# model = B.getModel(3).to(device)
+model=getCustomPretrained(resnet,features_len)
+model=model.to(device)
+
+# store best prediction in one epoch
+
+criterion = nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=learning_rate)
+
+
+
+
+
+
+# In[77]:
+
+
+
+
+# In[78]:
+
+
+def save_checkpoint(state,is_best,filename='./models/Unfreezecheckpoint.pth.tar'):
+    torch.save(state,filename)
+    if is_best:
+        shutil.copyfile(filename,'./models/Unfreezemodel_best.pth.tar')
+
+
+# In[79]:
+
+
+def train(train_loader,model,criterion,optimizer,epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    avgaccu =  AverageMeter()
+    
+    model.train()
+    
+    end = time.time()
+    for i,(images,labels) in enumerate(train_loader):
+        data_time.update(time.time()-end)
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        #output = model(images)
+        _,output = model(images)
+        # for resnet returns features and output
+        # print(type(output))
+        loss = criterion(output,labels)
+        
+        # top-k ? accuaracy 
+        # for now evaluating normal accuracy
+        acc = accuracy(output,labels)
+        
+        #loss.item() to get the loss value from loss tensor
+        losses.update(loss.item(), images.size(0))
+        avgaccu.update(acc,images.size(0))
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        logging.info('Epoch: [{0}][{1}/{2}]\t'
+              'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+              'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+              'Accuracy {acc.val:.4f} ({acc.avg:.4f})\t'.format(
+               epoch, i, len(train_loader), batch_time=batch_time,
+               data_time=data_time, loss=losses, acc=avgaccu))
+
+
+# In[80]:
+
+
+def validate(val_loader, model, criterion):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    avgaccu =  AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (input, target) in enumerate(val_loader):
+            input=input.to(device)
+            target =  target.to(device)
+            
+            # compute output
+            _,output = model(input)
+            
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc= accuracy(output, target)
+            losses.update(loss.item(), input.size(0))
+            avgaccu.update(acc,input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            logging.info('Validation: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc {acc.val:.3f} ({acc.avg:.3f})'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,acc=avgaccu))
+
+        logging.info(' * Acc {acc.avg:.3f}'
+              .format(acc=avgaccu))
+
+    return acc
+
+
+# In[81]:
+
+
+def accuracy(output,target):
+    with torch.no_grad():
+        batch_size =  target.size(0)
+        _, predicted = torch.max(output.data, 1)
+        total = target.size(0)
+        correct = (predicted == target).sum().item()
+        acc = correct/total
+    return acc
+
+
+# In[82]:
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+# In[83]:
+
+
+def test(test_loader,model):
+
+    # Test the model
+    model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            _,outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        logging.info('Test Accuracy of the model on the test images: {} %'.format(100 * correct / total))
+
+
+# In[84]:
+
+
+def adjust_learning_rate(optimizer,epoch,initLR):
+    '''Sets the learning rate to the initial LR decayed by 10 every 30 epoch'''
+    lr = initLR * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+# # Training
+
+# In[86]:
+
+
+best_acc = 0
+
+log_freq=10
+for epoch in range(num_epochs):
+    
+    adjust_learning_rate(optimizer,epoch,learning_rate)
+    
+    
+    train(train_loader,model,criterion,optimizer,epoch+1)
+    
+    
+    
+    acc =  validate(val_loader,model,criterion)
+    
+    
+    
+    is_best = acc > best_acc
+    
+    best_acc = max(acc,best_acc)
+    
+    
+    #saving the checkpoint if is_best is True
+    save_checkpoint({
+        'epoch':epoch+1,
+        'state_dict':model.state_dict(),
+        'best_acc':best_acc,
+        'optimizer':optimizer.state_dict(),
+    },is_best)
+    
+    if(epoch % log_freq==0):
+        test(test_loader=test_loader,model=model)
+        
+test(test_loader,model)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
