5976331209849d7e8d4814f5d821ae5a34c5271c
himanshuupreti <himanshu@github.com>
2018-11-21 07:31:58 +0530

    install from master

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip3 uninstall tensorflow --no-cache- -y
+pip3 uninstall tensorflow-gpu --no-cache-dir -y
+pip3 uninstall tensorboard --no-cache-dir -y
+pip3 uninstall tensorboardX --no-cache-dir -y
+pip3 install tensorflow --no-cache-dir --ignore-installed
+#pip3 install tensorflow-gpu --no-cache-dir --ignore-installed
+pip3 install tensorboard --no-cache-dir
+#pip3 install tensorboardX --no-cache-dir
